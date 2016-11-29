f6476e6c36dd02efebf71c648258d734f1c59619
Luna Kang <luna.kang.ca@gmail.com>
2016-11-29 10:23:19 -0800

    Seeding Data with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,39 @@
-require 'random_data'
-
 50.times do
-  Wiki.create!(
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+  User.create!(
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password
   )
 end
 
-wikis = Wiki.all
+users = User.all
+
+50.times do
+  Wiki.create!(
+    title:  Faker::Hacker.say_something_smart,
+    body:   Faker::Lorem.paragraph,
+    private: false,
+    user: users.sample
+  )
+end
+
+admin = User.create!(
+  email: "luna.kang.ca@gmail.com",
+  password: "helloworld",
+)
+admin.add_role :admin
+
+standard_member = User.create!(
+  email: "standard@bloc.com",
+  password: "helloworld",
+)
+standard_member.add_role :standard
+
+premium_member = User.create!(
+  email: "premium@bloc.com",
+  password: "helloworld",
+)
+premium_member.add_role :premium
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Wiki.count} wikis created"
