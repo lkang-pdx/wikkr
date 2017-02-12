ac4869bbbbf15d04108d19fe5fefd4a817f211c1
Luna Kang <luna.kang.ca@gmail.com>
2017-02-12 15:22:03 -0800

    JS Snippet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ var scrutinize = {};
 
     var request = new XMLHttpRequest();
 
-    request.open("POST", "tranquil-sands-50425.herokuapp.com/api/events", true);
+    request.open("POST", "http://tranquil-sands-50425.herokuapp.com/api/events", true);
 
     request.setRequestHeader('Content-Type', 'application/json');
 
